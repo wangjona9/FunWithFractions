a9ac71f2992ca9e6f4e16f857b4782ae3877d9e1
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-09-13 20:48:23 -0500

    Update BigFraction.java

--- BigFraction.java
+++ BigFraction.java
@@ -59,18 +59,24 @@ public class BigFraction {
    *
    * Warning! Not yet implemented.
    */
-  public BigFraction(String str) {
-    int slashIndex = str.indexOf("/");
-    
-    String numstr = str.substring(0, slashIndex);
-    String denomstr = str.substring(slashIndex + 1);
+  /**
+ * Build a new fraction by parsing a string.
+ */
+public BigFraction(String str) {
+    if (str.contains("/")) {
+        int slashIndex = str.indexOf("/");
+        
+        String numstr = str.substring(0, slashIndex);
+        String denomstr = str.substring(slashIndex + 1);
 
-    BigInteger num = new BigInteger(numstr);
-    BigInteger denom = new BigInteger(denomstr);
-
-    this.num = num;
-    this.denom = denom;
-  } // Fraction
+        this.num = new BigInteger(numstr);
+        this.denom = new BigInteger(denomstr);
+    } else {
+        this.num = new BigInteger(str);
+        this.denom = BigInteger.ONE; // Set denominator to 1 for single-digit integers
+    }
+}
+ // Fraction
 
   // +---------+------------------------------------------------------
   // | Methods |
@@ -133,18 +139,23 @@ public class BigFraction {
   /**
    * Convert this fraction to a string for ease of printing.
    */
- /* */// public String toString() {
+  public String toString() {
     // Special case: It's zero
-  //  if (this.num.equals(BigInteger.ZERO)) {
-   //   return "0";
-  //  } // if it's zero */
+    if (this.num.equals(BigInteger.ZERO)) {
+        return "0";
+    }
+
+    // Return the string representation of the numerator,
+    // a slash, and the string representation of the denominator
+    return this.num + "/" + this.denom;
+}
 
     // Lump together the string represention of the numerator,
     // a slash, and the string representation of the denominator
     // return this.num.toString().concat("/").concat(this.denom.toString());
   //  return this.num + "/" + this.denom;
  // } // toString()
-// class BigFraction
+// class Fraction
 
 
   public BigFraction multiply(BigFraction multiple) throws Exception{
@@ -154,38 +165,18 @@ public class BigFraction {
     return new BigFraction(newNumerator, newDenominator).simplify();
   }
   
-  public BigFraction divide(BigFraction divisor) throws Exception{
-      BigInteger newNumerator = this.num.divide(divisor.num);
-      BigInteger newDenominator = this.denom.divide(divisor.num);
-      
-      if (newDenominator.compareTo(BigInteger.ZERO) == 0) {
-          System.err.println("Can't divide by 0.");
-      }
-      
+  public BigFraction divide(BigFraction divisor) throws Exception {
+    BigInteger newNumerator = this.num.multiply(divisor.denom);
+    BigInteger newDenominator = this.denom.multiply(divisor.num);
+
+    if (newDenominator.compareTo(BigInteger.ZERO) == 0) {
+        System.err.println("Can't divide by 0.");
+    }
+
     return new BigFraction(newNumerator, newDenominator).simplify();
-  }
+}
   
-  public static void main(String[] args) throws Exception{
-    PrintWriter pen = new PrintWriter(System.out,true);
-    BigFraction BigFraction1 = new BigFraction(1,3);
-    BigFraction BigFraction2 = new BigFraction(1,5);
-    pen.println(BigFraction1.multiply(BigFraction2));
-
-    Counter c1 = new Counter();
-    Counter c2 = new Counter();
-    c1.increment();
-    c1.increment();
-    c2.increment();
-    pen.println("c1: " + c1.get());
-    pen.println("c2: " + c2.get());
-
-    c1.reset();
-    c2.reset();
-
-    pen.println("Reset c1: " + c1.get());
-    pen.println("Reset c2: " + c2.get());
-
-  }
+  
   // +---------+--------------------------------------------------------
   // | Methods |
   // +---------+
@@ -193,36 +184,3 @@ public class BigFraction {
   // ...
 
 } // class  BigFraction
-
-class Counter {
-  private int count;
-
-  public Counter() {
-    count = 0;
-  }
-
-  public Counter(int startingValue) {
-    count = 0;
-  }
-
-  public void reset() {
-    if (count != 0) {
-      count = 0;
-    }
-  }
-
-  public void increment() {
-    count++;
-  }
-
-  public int get() {
-    return count;
-  }
-
-  public String toString() {
-    return "<" + count + ">";
-  }
-}
-
-
-

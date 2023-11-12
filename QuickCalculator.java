c9dcf1b06dc10b28f90dacc42c548028243a9d76
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-12 13:25:06 -0600

    Update QuickCalculator.java

--- QuickCalculator.java
+++ QuickCalculator.java
@@ -7,22 +7,22 @@
  * September 2023
  */
 public class QuickCalculator {
-    public static void main(String[] args) {
-        BFCalculator calculator = new BFCalculator();
+  public static void main(String[] args) {
+    BFCalculator calculator = new BFCalculator();
 
-        for (String arg : args) { // For-each loop for the args
-            try { // Code that may throw exception
-                if (arg.startsWith("STORE")) {
-                    calculator.store(arg); // Call the store method for the args
-                } else {
-                    BigFraction result = calculator.evaluate(arg); // Call the evaluate method 
-                    if (result != null) {
-                        System.out.println(arg + " = " + result); // Print the result of the expression
-                    }
-                }
-            } catch (Exception e) { // Catch the exception
-                e.printStackTrace();
-            }
+    for (String arg : args) { // For-each loop for the args
+      try { // Code that may throw exception
+        if (arg.startsWith("STORE")) {
+          calculator.store(arg); // Call the store method for the args
+        } else {
+          BigFraction result = calculator.evaluate(arg); // Call the evaluate method 
+          if (result != null) {
+            System.out.println(arg + " = " + result); // Print the result of the expression
+          }
         }
+      } catch (Exception e) { // Catch the exception
+        e.printStackTrace();
+      }
     }
+  }
 }
